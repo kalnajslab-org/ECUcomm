1fd90b2aeb1cc569913ceb1a33210f50538ad82b
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2025-02-22 04:36:20 -0700

    Begin adding serialization functions

--- src/ECUReport.cpp
+++ src/ECUReport.cpp
@@ -1,4 +1,6 @@
 #include "ECUReport.h"
+#include "etl/bit_stream.h"
+#include "etl/vector.h"
 
 void bin_print(uint32_t n, uint8_t w)
 {
@@ -8,17 +10,51 @@ void bin_print(uint32_t n, uint8_t w)
     }
 }
 
-void ecu_report_init(ECUReport_t* ecu_report)
+void ecu_report_init(ECUReport_t& ecu_report)
 {
-    ecu_report->rev = ECU_REPORT_REV;
-    ecu_report->heat_on = 0;
-    ecu_report->v5 = 0;
-    ecu_report->v12 = 0;
-    ecu_report->v56 = 0;
-    ecu_report->gps_valid = 0;
-    ecu_report->gps_lat = 0;
-    ecu_report->gps_lon = 0;
-    ecu_report->gps_alt = 0;
+    ecu_report.rev = ECU_REPORT_REV;
+    ecu_report.heat_on = 0;
+    ecu_report.v5 = 0;
+    ecu_report.v12 = 0;
+    ecu_report.v56 = 0;
+    ecu_report.gps_valid = 0;
+    ecu_report.gps_lat = 0;
+    ecu_report.gps_lon = 0;
+    ecu_report.gps_alt = 0;
+}
+
+void add_ecu_health(float v5, float v12, float v56, float board_t, ECUReport_t& report) {
+    report.v5 = (uint16_t)(v5 * 100);
+    report.v12 = (uint16_t)(v12 * 100);
+    report.v56 = (uint16_t)(v56 * 100);
+    report.board_t = (uint8_t)((board_t + 100) * 10);
+}
+
+void add_gps(bool valid, double lat, double lon, double alt, ECUReport_t& report) {
+    report.gps_valid = valid;
+    report.gps_lat = (int32_t)(lat * 1.0e6);
+    report.gps_lon = (int32_t)(lon * 1.0e6);
+    report.gps_alt = (uint16_t)alt;
+}
+
+etl::array<uint8_t, ECU_REPORT_SIZE_BYTES> ecu_report_serialize(ECUReport_t& report) {
+
+    etl::array<uint8_t, ECU_REPORT_SIZE_BYTES> data;
+    etl::span<uint8_t> data_span(data.data(), data.size());
+    etl::bit_stream_writer writer(data_span, etl::endian::big);
+
+    writer.write_unchecked(report.rev, 4);
+    writer.write_unchecked(report.heat_on, 1);  // Heater on (bool)
+    writer.write_unchecked(report.v5, 9);  // V5*100  (0-511  : 0.00V to 5.11V) 
+    writer.write_unchecked(report.v12, 11);  // V12*100 (0-2047 : 0.00V to 20.47V)
+    writer.write_unchecked(report.v56, 13);  // V56*100 (0-8191 : 0.00V to 81.91V)
+    writer.write_unchecked(report.board_t, 11);  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.7C)
+    writer.write_unchecked(report.gps_valid, 1);  // GPS Valid (bool)
+    writer.write_unchecked(report.gps_lat, 32);  // GPS Latitude*1e6 (degrees*1e6)
+    writer.write_unchecked(report.gps_lon, 32);  // GPS Longitude*1e6 (degrees*1e6)
+    writer.write_unchecked(report.gps_alt, 16);  // GPS Altitude (meters)
+
+    return data;
 }
 
 void ecu_report_print(ECUReport_t* ecu_report)
--- src/ECUReport.h
+++ src/ECUReport.h
@@ -5,30 +5,38 @@
 #define ETL_NO_STL
 #define ETL_NO_INITIALIZER_LIST
 
-#include "etl/bit_stream.h"
 #include "etl/array.h"
 
 #define ECU_REPORT_REV 1
 
-#define ECU_REPORT_SIZE_BITS 96
-#define ECU_REPORT_SIZE_BYTES (ECU_REPORT_SIZE_BITS/8)
+#define ECU_REPORT_SIZE_BITS (4 + 1 + 9 + 11 + 13 + 11 + 1 + 32 + 32 + 16)
+
+// 4 + 1 + 9 + 11 + 13 + 11 + 1 + 32 + 32 + 16 = 130 bits
+// 130 bits / 8 bits/byte = 16.25 bytes
+// Round up by adding 1 byte. Perhaps there is a way to do this more elegantly with math in the #define?
+#define ECU_REPORT_SIZE_BYTES (1+ECU_REPORT_SIZE_BITS/8)
 
 struct ECUReport_t
 {
     uint8_t rev :        4;
     uint8_t heat_on :    1;  // Heater on (bool)
-    uint16_t v5 :        9;  // V5*100  (0-511  : 0.00V-5.11V)
-    uint16_t v12 :      11;  // V12*100 (0-2047 : 0.00V-20.47V)
-    uint16_t v56 :      13;  // V56*100 (0-8191 : 0.00V-81.91V)
-    uint8_t gps_valid :  1;
+    uint16_t v5 :        9;  // V5*100  (0-511  : 0.00V to 5.11V)
+    uint16_t v12 :      11;  // V12*100 (0-2047 : 0.00V to 20.47V)
+    uint16_t v56 :      13;  // V56*100 (0-8191 : 0.00V to 81.91V)
+    uint16_t board_t :  11;  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.7C)
+    uint8_t gps_valid :  1;  // GPS Valid (bool)
     int32_t gps_lat :   32;  // GPS Latitude*1e6 (degrees*1e6)
     int32_t gps_lon :   32;  // GPS Longitude*1e6 (degrees*1e6)
     uint16_t gps_alt:   16;  // GPS Altitude (meters)
 };
 
 void bin_print(uint32_t n, uint8_t w=8);
-void ecu_report_init(ECUReport_t* report);
+void ecu_report_init(ECUReport_t& report);
 void ecu_report_print(ECUReport_t* ecu_report);
+void add_ecu_health(float v5, float v12, float v56, float board_t, ECUReport_t& report);
+void add_gps(bool valid, double lat, double lon, double alt, ECUReport_t& report);
+etl::array<uint8_t, ECU_REPORT_SIZE_BYTES> ecu_report_serialize(ECUReport_t& report);
+
 
 #endif //_ECU_REPORT_H_
 
