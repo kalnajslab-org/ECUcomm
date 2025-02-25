efa5f94516b1fe17569f7b0f913d7cbb14d6ac9b
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2025-02-25 10:51:15 -0700

    Add RS41 values to ECUReport

--- src/ECUReport.cpp
+++ src/ECUReport.cpp
@@ -25,6 +25,12 @@ void ecu_report_init(ECUReport_t& ecu_report)
     ecu_report.gps_sats = 0;
     ecu_report.gps_hdop = 0;
     ecu_report.gps_age_secs = 255;
+    ecu_report.board_t = 0;
+    ecu_report.rs41_airt = 0;
+    ecu_report.rs41_hum = 0;
+    ecu_report.rs41_hst = 0;
+    ecu_report.rs41_pres = 0;
+    ecu_report.rs41_pcb_h = 0;
 }
 
 void add_status(bool heat_on, ECUReport_t& report) {
@@ -32,20 +38,56 @@ void add_status(bool heat_on, ECUReport_t& report) {
 }
 
 void add_ecu_health(float v5, float v12, float v56, float board_t, ECUReport_t& report) {
-    report.v5 = (uint16_t)(v5 * 100);
-    report.v12 = (uint16_t)(v12 * 100);
-    report.v56 = (uint16_t)(v56 * 100);
-    report.board_t = (uint16_t)((board_t + 100) * 10);
+    report.v5 = v5 * 100;
+    report.v12 = v12 * 100;
+    report.v56 = v56 * 100;
+    report.board_t = (board_t + 100) * 10;
 }
 
 void add_gps(bool valid, double lat, double lon, double alt, uint sats, uint hdop, uint age_secs, ECUReport_t& report) {
     report.gps_valid = valid;
-    report.gps_lat      = (int32_t)(lat * 1.0e6);
-    report.gps_lon      = (int32_t)(lon * 1.0e6);
-    report.gps_alt      = (uint16_t)alt;
+    report.gps_lat      = lat * 1.0e6;
+    report.gps_lon      = lon * 1.0e6;
+    report.gps_alt      = alt;
     report.gps_sats     = (sats > 31)      ? 31 : sats;
     report.gps_hdop     = (hdop > 255)     ? 255 : hdop;
     report.gps_age_secs = (age_secs > 255) ? 255 : age_secs;
+}
+
+void add_rs41(float airt, float hum, float hst, float pres, bool pcb_h, ECUReport_t& report) {
+    if (airt < -100.0) {
+        report.rs41_airt = 0;
+    } else if (airt > 63.83) {
+        report.rs41_airt = 16383;
+    } else {
+        report.rs41_airt = (airt + 100) * 100;
+    }
+
+    if  (hum < 0.0) {
+        report.rs41_hum = 0;
+    } else if (hum > 102.3) {
+        report.rs41_hum = 1023;
+    } else {
+        report.rs41_hum = hum * 10;
+    }
+
+    if (hst < -100.0) {
+        report.rs41_hst = 0;
+    } else if (hst > 125.0) {
+        report.rs41_hst = 255;
+    } else {
+        report.rs41_hst = hst + 100;
+    }
+
+    if (pres < 0.0) {
+        report.rs41_pres = 0;
+    } else if (pres > 1310.71) {
+        report.rs41_pres = 131071;
+    } else {
+        report.rs41_pres = pres * 100;
+    }
+
+    report.rs41_pcb_h = pcb_h;
 }
 
 ECUReportBytes_t ecu_report_serialize(ECUReport_t& report) {
@@ -54,19 +96,24 @@ ECUReportBytes_t ecu_report_serialize(ECUReport_t& report) {
     etl::span<uint8_t> data_span(data.data(), data.size());
     etl::bit_stream_writer writer(data_span, etl::endian::big);
 
-    writer.write_unchecked(report.rev, 4);
-    writer.write_unchecked(report.heat_on, 1);  // Heater on (bool)
-    writer.write_unchecked(report.v5, 9);  // V5*100  (0-511  : 0.00V to 5.11V) 
-    writer.write_unchecked(report.v12, 11);  // V12*100 (0-2047 : 0.00V to 20.47V)
-    writer.write_unchecked(report.v56, 13);  // V56*100 (0-8191 : 0.00V to 81.91V)
-    writer.write_unchecked(report.board_t, 11);  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.7C)
-    writer.write_unchecked(report.gps_valid, 1);  // GPS Valid (bool)
-    writer.write_unchecked(report.gps_lat, 32);  // GPS Latitude*1e6 (degrees*1e6)
-    writer.write_unchecked(report.gps_lon, 32);  // GPS Longitude*1e6 (degrees*1e6)
-    writer.write_unchecked(report.gps_alt, 16);  // GPS Altitude (meters)
-    writer.write_unchecked(report.gps_sats, 5);  // Number of satellites n (0 to 31)
-    writer.write_unchecked(report.gps_hdop, 8);  // HDOP m (0 to 255) 255 = greater than 254
-    writer.write_unchecked(report.gps_age_secs, 8);  // Age of GPS data in seconds (0 to 255) 255 = greater than 254
+    writer.write_unchecked(report.rev,          4);
+    writer.write_unchecked(report.heat_on,      1);
+    writer.write_unchecked(report.v5,           9);
+    writer.write_unchecked(report.v12,         11);
+    writer.write_unchecked(report.v56,         13);
+    writer.write_unchecked(report.board_t,     11);
+    writer.write_unchecked(report.gps_valid,    1);
+    writer.write_unchecked(report.gps_lat,     32);
+    writer.write_unchecked(report.gps_lon,     32);
+    writer.write_unchecked(report.gps_alt,     16);
+    writer.write_unchecked(report.gps_sats,     5);
+    writer.write_unchecked(report.gps_hdop,     8);
+    writer.write_unchecked(report.gps_age_secs, 8);
+    writer.write_unchecked(report.rs41_airt,   14);
+    writer.write_unchecked(report.rs41_hum,    10);
+    writer.write_unchecked(report.rs41_hst,     8);
+    writer.write_unchecked(report.rs41_pres,   17);
+    writer.write_unchecked(report.rs41_pcb_h,   1);
 
     return data;
 }
@@ -76,19 +123,24 @@ ECUReport_t ecu_report_deserialize(ECUReportBytes_t& data) {
     etl::bit_stream_reader reader(data_span, etl::endian::big);
 
     ECUReport_t report;
-    report.rev = reader.read_unchecked<uint8_t>(4);
-    report.heat_on = reader.read_unchecked<uint8_t>(1);  // Heater on (bool)
-    report.v5 = reader.read_unchecked<uint16_t>(9);  // V5*100  (0-511  : 0.00V to 5.11V) 
-    report.v12 = reader.read_unchecked<uint16_t>(11);  // V12*100 (0-2047 : 0.00V to 20.47V)
-    report.v56 = reader.read_unchecked<uint16_t>(13);  // V56*100 (0-8191 : 0.00V to 81.91V)
-    report.board_t = reader.read_unchecked<uint16_t>(11);  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.7C)
-    report.gps_valid = reader.read_unchecked<uint8_t>(1);  // GPS Valid (bool)
-    report.gps_lat = reader.read_unchecked<int32_t>(32);  // GPS Latitude*1e6 (degrees*1e6)
-    report.gps_lon = reader.read_unchecked<int32_t>(32);  // GPS Longitude*1e6 (degrees*1e6)
-    report.gps_alt = reader.read_unchecked<uint16_t>(16);  // GPS Altitude (meters)
-    report.gps_sats = reader.read_unchecked<uint8_t>(5);  // Number of satellites n (0 to 31)
-    report.gps_hdop = reader.read_unchecked<uint8_t>(8);  // HDOP m (0 to 255) 255 = greater than 254
-    report.gps_age_secs = reader.read_unchecked<uint8_t>(8);  // Age of GPS data in seconds (0 to 255) 255 = greater than 254
+    report.rev = reader.read_unchecked<uint8_t>         (4);
+    report.heat_on = reader.read_unchecked<uint8_t>     (1); // Heater on (bool)
+    report.v5 = reader.read_unchecked<uint16_t>         (9); // V5*100  (0-511  : 0.00V to 5.11V) 
+    report.v12 = reader.read_unchecked<uint16_t>       (11); // V12*100 (0-2047 : 0.00V to 20.47V)
+    report.v56 = reader.read_unchecked<uint16_t>       (13); // V56*100 (0-8191 : 0.00V to 81.91V)
+    report.board_t = reader.read_unchecked<uint16_t>   (11); // (Board temperature+100)*10 (0-2047 : -100.0C to 104.7C)
+    report.gps_valid = reader.read_unchecked<uint8_t>   (1); // GPS Valid (bool)
+    report.gps_lat = reader.read_unchecked<int32_t>    (32); // GPS Latitude*1e6 (degrees*1e6)
+    report.gps_lon = reader.read_unchecked<int32_t>    (32); // GPS Longitude*1e6 (degrees*1e6)
+    report.gps_alt = reader.read_unchecked<uint16_t>   (16); // GPS Altitude (meters)
+    report.gps_sats = reader.read_unchecked<uint8_t>    (5); // Number of satellites n (0 to 31)
+    report.gps_hdop = reader.read_unchecked<uint8_t>    (8); // HDOP m (0 to 255) 255 = greater than 254
+    report.gps_age_secs = reader.read_unchecked<uint8_t>(8); // Age of GPS data in seconds (0 to 255) 255 = greater than 254
+    report.rs41_airt = reader.read_unchecked<uint16_t> (14); // (RS41 Air Temperature+100)*100 (0-16383 : -100.00C to 63.83C)
+    report.rs41_hum = reader.read_unchecked<uint16_t>  (10); // RS41 Humidity*10 (0-1023 : 0.0% to 102.3%)
+    report.rs41_hst = reader.read_unchecked<uint8_t>    (8); // RS41 Humidity Sensor Temperature+100 (0-255 : -100C to 125C)
+    report.rs41_pres = reader.read_unchecked<uint32_t> (17); // RS41 Pressure*100 (0-131071 : 0.0hPa to 1310.71hPa) (should we do log10?)
+    report.rs41_pcb_h = reader.read_unchecked<uint8_t>  (1); // RS41 PCB Heater On (bool)
 
     return report;
 }
@@ -96,17 +148,22 @@ ECUReport_t ecu_report_deserialize(ECUReportBytes_t& data) {
 void ecu_report_print(ECUReport_t& ecu_report, bool print_bin=false)
 {
     SerialUSB.println("ECU Report:");
-    SerialUSB.print("rev: "); if (print_bin) ecu_bin_print(ecu_report.rev, 4); SerialUSB.print(String(ecu_report.rev)); SerialUSB.println();
-    SerialUSB.print("heat_on: "); if (print_bin) ecu_bin_print(ecu_report.heat_on, 1); SerialUSB.print(ecu_report.heat_on?"True":"False"); SerialUSB.println();
-    SerialUSB.print("v5: "); if (print_bin) ecu_bin_print(ecu_report.v5, 9); SerialUSB.print(String(ecu_report.v5/100.0, 2) + "V"); SerialUSB.println();
-    SerialUSB.print("v12: "); if (print_bin) ecu_bin_print(ecu_report.v12, 11); SerialUSB.print(String(ecu_report.v12/100.0, 2) + "V"); SerialUSB.println();
-    SerialUSB.print("v56: "); if (print_bin) ecu_bin_print(ecu_report.v56, 13); SerialUSB.print(String(ecu_report.v56/100.0, 2) + "V"); SerialUSB.println();
-    SerialUSB.print("board_t: "); if (print_bin) ecu_bin_print(ecu_report.board_t, 11); SerialUSB.print(String((ecu_report.board_t/10.0)-100.0, 1) + "degC"); SerialUSB.println();
-    SerialUSB.print("gps_valid: "); if (print_bin) ecu_bin_print(ecu_report.gps_valid, 1); SerialUSB.print(ecu_report.gps_valid?"True":"False"); SerialUSB.println();
-    SerialUSB.print("gps_lat: "); if (print_bin) ecu_bin_print(ecu_report.gps_lat, 32); SerialUSB.print(String(ecu_report.gps_lat/1.0e6, 6) + "deg"); SerialUSB.println();
-    SerialUSB.print("gps_lon: "); if (print_bin) ecu_bin_print(ecu_report.gps_lon, 32); SerialUSB.print(String(ecu_report.gps_lon/1.0e6, 6) + "deg"); SerialUSB.println();
-    SerialUSB.print("gps_alt: "); if (print_bin) ecu_bin_print(ecu_report.gps_alt, 16); SerialUSB.print(String(ecu_report.gps_alt*1.0, 1) + "m"); SerialUSB.println();
-    SerialUSB.print("gps_sats: "); if (print_bin) ecu_bin_print(ecu_report.gps_sats, 5); SerialUSB.print(String(ecu_report.gps_sats)); SerialUSB.println();
-    SerialUSB.print("gps_hdop: "); if (print_bin) ecu_bin_print(ecu_report.gps_hdop, 8); SerialUSB.print(String(ecu_report.gps_hdop) + "m"); SerialUSB.println();
+    SerialUSB.print("rev: "); if (print_bin) ecu_bin_print(ecu_report.rev,                   4); SerialUSB.print(String(ecu_report.rev)); SerialUSB.println();
+    SerialUSB.print("heat_on: "); if (print_bin) ecu_bin_print(ecu_report.heat_on,           1); SerialUSB.print(ecu_report.heat_on?"True":"False"); SerialUSB.println();
+    SerialUSB.print("v5: "); if (print_bin) ecu_bin_print(ecu_report.v5,                     9); SerialUSB.print(String(ecu_report.v5/100.0, 2) + "V"); SerialUSB.println();
+    SerialUSB.print("v12: "); if (print_bin) ecu_bin_print(ecu_report.v12,                  11); SerialUSB.print(String(ecu_report.v12/100.0, 2) + "V"); SerialUSB.println();
+    SerialUSB.print("v56: "); if (print_bin) ecu_bin_print(ecu_report.v56,                  13); SerialUSB.print(String(ecu_report.v56/100.0, 2) + "V"); SerialUSB.println();
+    SerialUSB.print("board_t: "); if (print_bin) ecu_bin_print(ecu_report.board_t,          11); SerialUSB.print(String((ecu_report.board_t/10.0)-100.0, 1) + "degC"); SerialUSB.println();
+    SerialUSB.print("gps_valid: "); if (print_bin) ecu_bin_print(ecu_report.gps_valid,       1); SerialUSB.print(ecu_report.gps_valid?"True":"False"); SerialUSB.println();
+    SerialUSB.print("gps_lat: "); if (print_bin) ecu_bin_print(ecu_report.gps_lat,          32); SerialUSB.print(String(ecu_report.gps_lat/1.0e6, 6) + "deg"); SerialUSB.println();
+    SerialUSB.print("gps_lon: "); if (print_bin) ecu_bin_print(ecu_report.gps_lon,          32); SerialUSB.print(String(ecu_report.gps_lon/1.0e6, 6) + "deg"); SerialUSB.println();
+    SerialUSB.print("gps_alt: "); if (print_bin) ecu_bin_print(ecu_report.gps_alt,          16); SerialUSB.print(String(ecu_report.gps_alt*1.0, 1) + "m"); SerialUSB.println();
+    SerialUSB.print("gps_sats: "); if (print_bin) ecu_bin_print(ecu_report.gps_sats,         5); SerialUSB.print(String(ecu_report.gps_sats)); SerialUSB.println();
+    SerialUSB.print("gps_hdop: "); if (print_bin) ecu_bin_print(ecu_report.gps_hdop,         8); SerialUSB.print(String(ecu_report.gps_hdop) + "m"); SerialUSB.println();
     SerialUSB.print("gps_age_secs: "); if (print_bin) ecu_bin_print(ecu_report.gps_age_secs, 8); SerialUSB.print(String(ecu_report.gps_age_secs) + "s"); SerialUSB.println();
+    SerialUSB.print("rs41_airt: "); if (print_bin) ecu_bin_print(ecu_report.rs41_airt,      14); SerialUSB.print(String((ecu_report.rs41_airt/100.0)-100.0, 2) + "degC"); SerialUSB.println();
+    SerialUSB.print("rs41_hum: "); if (print_bin) ecu_bin_print(ecu_report.rs41_hum,        10); SerialUSB.print(String(ecu_report.rs41_hum/10.0, 1) + "%"); SerialUSB.println();
+    SerialUSB.print("rs41_hst: "); if (print_bin) ecu_bin_print(ecu_report.rs41_hst,         8); SerialUSB.print(String((ecu_report.rs41_hst/1.0)-100.0, 1) + "degC"); SerialUSB.println();
+    SerialUSB.print("rs41_pres: "); if (print_bin) ecu_bin_print(ecu_report.rs41_pres,      17); SerialUSB.print(String(ecu_report.rs41_pres/100.0, 2) + "hPa"); SerialUSB.println();
+    SerialUSB.print("rs41_pcb_h: "); if (print_bin) ecu_bin_print(ecu_report.rs41_pcb_h,     1); SerialUSB.print(ecu_report.rs41_pcb_h?"True":"False"); SerialUSB.println();
 }
--- src/ECUReport.h
+++ src/ECUReport.h
@@ -9,9 +9,9 @@
 
 #define ECU_REPORT_REV 1
 
-#define ECU_REPORT_SIZE_BITS (4 + 1 + 9 + 11 + 13 + 11 + 1 + 32 + 32 + 16 + 5 + 8 + 8)
-// 4 + 1 + 9 + 11 + 13 + 11 + 1 + 32 + 32 + 16 + 5 + 8 + 8 = 139 bits
-// 139 bits / 8 bits/byte = 17.375 bytes
+#define ECU_REPORT_SIZE_BITS (4+1+9+11+13+11+1+32+32+16+5+8+8+14+10+8+17+1)
+// 4+1+9+11+13+11+1+32+32+16+5+8+8+14+10+8+17+1 = 201 bits
+// 201 bits / 8 bits/byte = 25.125 bytes
 // Round up by adding 1 byte. Perhaps there is a way to do this more elegantly with math in the #define?
 #define ECU_REPORT_SIZE_BYTES (1+ECU_REPORT_SIZE_BITS/8)
 
@@ -22,31 +22,37 @@
 // *** There are several functions in ECUReport.cpp that must be editied, and exactly match the data structure defined here. ***
 struct ECUReport_t
 {
-    uint8_t rev :        4;
-    uint8_t heat_on :    1;  // Heater on (bool)
+    uint8_t  rev :       4;
+    uint8_t  heat_on :   1;  // Heater on (bool)
     uint16_t v5 :        9;  // V5*100  (0-511  : 0.00V to 5.11V)
     uint16_t v12 :      11;  // V12*100 (0-2047 : 0.00V to 20.47V)
     uint16_t v56 :      13;  // V56*100 (0-8191 : 0.00V to 81.91V)
-    uint16_t board_t :  11;  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.7C)
-    uint8_t gps_valid :  1;  // GPS Valid (bool)
-    int32_t gps_lat :   32;  // GPS Latitude*1e6 (degrees*1e6)
-    int32_t gps_lon :   32;  // GPS Longitude*1e6 (degrees*1e6)
-    uint16_t gps_alt:   16;  // GPS Altitude (meters)
-    uint8_t gps_sats:    5;  // Number of satellites n (0 to 31)
-    uint8_t gps_hdop:    8;  // HDOP m (0 to 255) 255 = greater than 254
-    uint8_t gps_age_secs:8;  // Age of GPS data in seconds (0 to 255) 255 = greater than 254
+    uint16_t board_t :  11;  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.8C)
+    uint8_t  gps_valid : 1;  // GPS Valid (bool)
+    int32_t  gps_lat :   32;  // GPS Latitude*1e6 (degrees*1e6)
+    int32_t  gps_lon :   32;  // GPS Longitude*1e6 (degrees*1e6)
+    uint16_t gps_alt:    16;  // GPS Altitude (meters)
+    uint8_t  gps_sats:    5;  // Number of satellites n (0 to 31)
+    uint8_t  gps_hdop:    8;  // HDOP m (0 to 255) 255 = greater than 254
+    uint8_t  gps_age_secs:8;  // Age of GPS data in seconds (0 to 255) 255 = greater than 254
+    uint16_t rs41_airt : 14;  // (RS41 Air Temperature+100)*100 (0-16383 : -100.00C to 63.83C) 
+    uint16_t rs41_hum:   10;  // RS41 Humidity*10 (0-1023 : 0.0% to 102.3%)
+    uint8_t  rs41_hst:    8;  // RS41 Humidity Sensor Temperature+100 (0-255 : -100C to 125C)
+    uint32_t rs41_pres:  17;  // RS41 Pressure*100 (0-131071 : 0.0hPa to 1310.71hPa) (should we do log10?)
+    uint8_t  rs41_pcb_h:  1;  // RS41 PCB Heater On (bool)
 };
 
 // A byte array to hold the serialized ECUReport_t data structure.
 typedef etl::array<uint8_t, ECU_REPORT_SIZE_BYTES> ECUReportBytes_t;
 
-void ecu_bin_print(uint32_t n, uint8_t w=8);
 void ecu_report_init(ECUReport_t& report);
-void ecu_report_print(ECUReport_t& ecu_report, bool print_bin=false);
 void add_ecu_health(float v5, float v12, float v56, float board_t, ECUReport_t& report);
 void add_status(bool heat_on, ECUReport_t& report);
 void add_gps(bool valid, double lat, double lon, double alt, uint sats, uint hdop, uint age_secs, ECUReport_t& report);
+void add_rs41(float airt, float hum, float hst, float pres, bool pcb_h, ECUReport_t& report);
 ECUReportBytes_t ecu_report_serialize(ECUReport_t& report);
 ECUReport_t ecu_report_deserialize(ECUReportBytes_t& data);
+void ecu_report_print(ECUReport_t& ecu_report, bool print_bin=false);
+void ecu_bin_print(uint32_t n, uint8_t w=8);
 
 #endif //_ECU_REPORT_H_
