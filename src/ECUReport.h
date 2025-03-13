502eec845d26805fa0beab4c488c5109c4e1e2d0
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2025-03-12 19:07:01 -0600

    Add temperature setpoint to ECUReport

--- src/ECUReport.h
+++ src/ECUReport.h
@@ -7,7 +7,7 @@
 
 #include "etl/array.h"
 
-#define ECU_REPORT_REV 1
+#define ECU_REPORT_REV 2
 
 // *** NOTE: There are several functions in ECUReport.cpp that must be updated whenever 
 // the report is modified, and they must exactly match the data structure defined here. 
@@ -29,8 +29,8 @@
 // we have to define it here.
 // *** Correct ECU_REPORT_SIZE_BITS when ECUReport_t is modified ***
 // (Use copliot to create this sum by by propmting: "sum of bitfield sizes in ECUReport_t")
-#define ECU_REPORT_SIZE_BITS (4 + 1 + 9 + 11 + 13 + 11 + 8 + 1 + 32 + 32 + 16 + 5 + 17 + 25 + 8 + 1 + 14 + 10 + 8 + 17 + 1 + 12 + 24 + 24)
-// Total bits: 304 bits = 38 bytes
+#define ECU_REPORT_SIZE_BITS (4 + 1 + 9 + 11 + 13 + 11 + 8 + 8 + 1 + 32 + 32 + 16 + 5 + 17 + 25 + 8 + 1 + 14 + 10 + 8 + 17 + 1 + 12 + 24 + 24)
+// Total bits: 312 bits = 39 bytes
 // Round up to full byte adding 1. Perhaps there is a way to do this more elegantly with math in the #define?
 #define ECU_REPORT_SIZE_BYTES (1+ECU_REPORT_SIZE_BITS/8)
 
@@ -39,30 +39,31 @@
 // In compliance with C bitfield rules, the bitfield type must be large enough to hold the bitfield.
 struct ECUReport_t
 {
-    uint8_t  rev :        4;
-    uint8_t  heat_on :    1;  // Heater on (bool)
-    uint16_t v5 :         9;  // V5*100  (0-511  : 0.00V to 5.11V)
-    uint16_t v12 :       11;  // V12*100 (0-2047 : 0.00V to 20.47V)
-    uint16_t v56 :       13;  // V56*100 (0-8191 : 0.00V to 81.91V)
-    uint16_t board_t :   11;  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.8C)
-    uint8_t  switch_mA :  8;  // Switch current (mA): (0ma - 255mA)
-    uint8_t  gps_valid :  1;  // GPS Valid (bool)
-    int32_t  gps_lat :   32;  // GPS Latitude*1e6 (degrees*1e6)
-    int32_t  gps_lon :   32;  // GPS Longitude*1e6 (degrees*1e6)
-    uint16_t gps_alt:    16;  // GPS Altitude (meters)
-    uint8_t  gps_sats:    5;  // Number of satellites n (0 to 31)
-    uint32_t gps_date:   17;  // GPS Date (MMDDYY - Year is 20YY)
-    uint32_t gps_time:   25;  // GPS Time (HHMMSSSS - Seconds are in 100ths)
-    uint8_t  gps_age_secs:8;  // Age of GPS data in seconds (0 to 255) 255 = greater than 254
-    uint8_t  rs41_valid:  1;  // RS41 data valid (bool)
-    uint16_t rs41_airt : 14;  // (RS41 Air Temperature+100)*100 (0-16383 : -100.00C to 63.83C) 
-    uint16_t rs41_hum:   10;  // RS41 Humidity*10 (0-1023 : 0.0% to 102.3%)
-    uint8_t  rs41_hst:    8;  // RS41 Humidity Sensor Temperature+100 (0-255 : -100C to 125C)
-    uint32_t rs41_pres:  17;  // RS41 Pressure*100 (0-131071 : 0.0hPa to 1310.71hPa) (should we do log10?)
-    uint8_t  rs41_pcb_h:  1;  // RS41 PCB Heater On (bool)
-    uint16_t tsen_airt:  12;  // Raw
-    uint32_t tsen_ptemp: 24;  // Raw
-    uint32_t tsen_pres:  24;  // Raw
+    uint8_t  rev :          4;
+    uint8_t  heat_on :      1;  // Heater on (bool)
+    uint16_t v5 :           9;  // V5*100  (0-511  : 0.00V to 5.11V)
+    uint16_t v12 :         11;  // V12*100 (0-2047 : 0.00V to 20.47V)
+    uint16_t v56 :         13;  // V56*100 (0-8191 : 0.00V to 81.91V)
+    uint16_t board_t :     11;  // (Board temperature+100)*10 (0-2047 : -100.0C to 104.8C)
+    uint8_t  temp_setpoint: 8;  // Setpoint temperature+100: (0-255 : -100C to 155C) 
+    uint8_t  switch_mA :    8;  // Switch current (mA): (0ma - 255mA)
+    uint8_t  gps_valid :    1;  // GPS Valid (bool)
+    int32_t  gps_lat :     32;  // GPS Latitude*1e6 (degrees*1e6)
+    int32_t  gps_lon :     32;  // GPS Longitude*1e6 (degrees*1e6)
+    uint16_t gps_alt:      16;  // GPS Altitude (meters)
+    uint8_t  gps_sats:      5;  // Number of satellites n (0 to 31)
+    uint32_t gps_date:     17;  // GPS Date (MMDDYY - Year is 20YY)
+    uint32_t gps_time:     25;  // GPS Time (HHMMSSSS - Seconds are in 100ths)
+    uint8_t  gps_age_secs:  8;  // Age of GPS data in seconds (0 to 255) 255 = greater than 254
+    uint8_t  rs41_valid:    1;  // RS41 data valid (bool)
+    uint16_t rs41_airt :   14;  // (RS41 Air Temperature+100)*100 (0-16383 : -100.00C to 63.83C) 
+    uint16_t rs41_hum:     10;  // RS41 Humidity*10 (0-1023 : 0.0% to 102.3%)
+    uint8_t  rs41_hst:      8;  // RS41 Humidity Sensor Temperature+100 (0-255 : -100C to 125C)
+    uint32_t rs41_pres:    17;  // RS41 Pressure*100 (0-131071 : 0.0hPa to 1310.71hPa) (should we do log10?)
+    uint8_t  rs41_pcb_h:    1;  // RS41 PCB Heater On (bool)
+    uint16_t tsen_airt:    12;  // Raw
+    uint32_t tsen_ptemp:   24;  // Raw
+    uint32_t tsen_pres:    24;  // Raw
 };
 
 // A byte array to hold the serialized ECUReport_t data structure.
@@ -75,7 +76,7 @@ void ecu_report_init(ECUReport_t& report);
 // Add ECU health to an ECUReport_t.
 void add_ecu_health(float v5, float v12, float v56, float board_t, float switch_mA, ECUReport_t& report);
 // Add statuses to an ECUReport_t.
-void add_status(bool heat_on, ECUReport_t& report);
+void add_status(bool heat_on, uint8_t temp_setpoint, ECUReport_t& report);
 // Add GPS data to an ECUReport_t.
 void add_gps(bool valid, double lat, double lon, double alt, uint sats, uint32_t date, uint32_t time, uint age_secs, ECUReport_t& report);
 // Add RS41 data to an ECUReport_t.
